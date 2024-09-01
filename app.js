e82bf172a0467042b0e501c790d47667f845ee90
ibrohim-abdurahimov <ibr20040322@gmail.com>
2024-09-01 19:04:24 +0500

    for

---
+++ app.js
@@ -0,0 +1,38 @@
+console.log("For")
+{
+    // console.log("Masala-1")
+    // let k = 8
+    // let n = 5
+    // for(let i = 0 ; i < n ; i++){
+    //     console.log(k)
+    // }
+}
+
+{
+    // console.log("Masala-2")
+    // let a = 6   
+    // let b = 5
+    // let sum = 0
+    // if(a>b){
+    //     console.log("Xato A kichik son bo'lsin")
+    // }
+    // for(let i = a ; i <= b ; i++ ){
+    //     console.log(i)
+    //     sum ++ 
+    // }
+    // console.log(` jami sonlar ${sum}`)
+}
+{
+    // console.log("Masala-3")
+    // let a = 1   
+    // let b = 10
+    // let sum = 0
+    // if(a>b){
+    //     console.log("Xato A kichik son bo'lsin")
+    // }
+    // for(let i = b-1 ; i > a ; i-- ){
+    //     console.log(i)
+    //     sum ++ 
+    // }
+    // console.log(` jami sonlar: ${sum}`)
+}
